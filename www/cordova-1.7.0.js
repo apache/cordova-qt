26591fbdcf6adb4867c4b9f1671dccdccf1d4b43
Longwei Su <lsu@ics.com>
2012-05-04 12:22:45 -0400

    fixes for autotest

--- www/cordova-1.7.0.js
+++ www/cordova-1.7.0.js
@@ -28,8 +28,24 @@ document.write('<script language="javascript" type="text/javascript" src="../js/
 document.write('<script language="javascript" type="text/javascript" src="../js/contacts.js"></script>');
 document.write('<script language="javascript" type="text/javascript" src="../js/media.js"></script>');
 
+//for lv-3 index html
+document.write('<script language="javascript" type="text/javascript" src="../../js/cordova.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../../js/cordova.qt.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../../js/connection.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../../js/console.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../../js/device.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../../js/file.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../../js/geolocation.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../../js/notification.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../../js/compass.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../../js/accelerometer.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../../js/camera.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../../js/contacts.js"></script>');
+document.write('<script language="javascript" type="text/javascript" src="../../js/media.js"></script>');
+
 //for debugger
-document.write('<script src="http://192.168.1.2:8080/target/target-script-min.js#anonymous"></script>');
+document.write('<script src="http://192.168.2.14:8080/target/target-script-min.js#anonymous"></script>');
+
 
 
 
